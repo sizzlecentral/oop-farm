1ca2c45fd6696fab19ebbabf5dc749c7c6fe0058
Don Cooper <don_cooper@hotmail.com>
2017-05-26 15:06:05 -0400

    Farm and Product classes created

---
+++ oop-farm-products.rb
@@ -0,0 +1,13 @@
+# OOP Farm Products
+# -----------------
+
+class Products
+
+  def initialize(name, food_per_hectare)
+    @name = name
+    @food_per_hectare = food_per_hectare
+  end
+
+end
+
+# ----- End of Products class ----- #
--- oop-farm.rb
+++ oop-farm.rb
@@ -1,2 +1,23 @@
 # OOP Farm
 # --------
+
+class Farm
+
+  require_relative 'oop-farm-products'
+  @@total_food = []
+
+# ----- Class Methods ----- #
+
+
+
+
+# ----- Instance Methods ----- #
+
+
+
+end
+
+# ----- End of Farm class ----- #
+
+corn = Product.new("Corn", 20)
+wheat = Product.new("Wheat", 30)
